c0b3f1b52768b93abfec39d7eb1160dd84dc0388
Your Name <arssonist@yahoo.com>
2017-02-19 14:51:16 -0500

    make squares clickable; state is toggled btw two things in Board, while sqaure is functional stateless

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => alert('click')}>
-      {this.props.value}
-      </button>
-    );
-  }
+
+//STATE
+// - add the constructor to use state
+// - subclasses need super()
+// - event hooks into setState
+// - this.state is passed in instead of props
+
+// First intance of Sqaure as a regular Component that handles state
+// class Square extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//     value: null,
+//     };
+//   }
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//     </button>
+//     );
+//   }
+// }
+
+// Change the Sqaure to a func stateless comp, that only accepts props and no state
+function Square(props){
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  )
 }
+
 class Board extends React.Component {
-  renderSquare(i){
-    return <Square />;
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+      xisNext: true,
+    };
   }
+  renderSquare(i){
+    // bring state down out of square into board
+     return <Square
+      //  return and <Component Must be on same line
+    value={this.state.squares[i]}
+  onClick={() => this.handleClick(i)} />
+
+
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+ });
+    }
   render() {
-  const status = "Next Player: X";
+  const status = 'Next Player: ' + (this.setState.xIsNext ? 'x' : 'O') ;
     return (
         <div className="board">
         <div className="status">{status}</div>
